9e0849c73c62bd46dedf0b40622a2457c15a30c9
Saarrah Rimaz <saarrahisthikar@gmail.com>
2019-02-23 15:37:22 +0400

    initial skelton

---
+++ src/main/java/com/saarrah/haleemas/HaleemasApplication.java
@@ -0,0 +1,13 @@
+package com.saarrah.haleemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaleemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaleemasApplication.class, args);
+	}
+
+}
